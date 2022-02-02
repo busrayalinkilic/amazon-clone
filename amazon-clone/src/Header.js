ba693b378689882ed26ee62f2fe9784cb8a33757
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2022-02-02 15:11:27 +0300

    başlangıç:02.02.22

---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket"
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header_logo "
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+      <div className="header_search">
+        <input className="header_searchInput" type="text"></input>
+        <SearchIcon className="header_searchIcon" />
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne"> Hello Guest</span>
+          <span className="header_optionLineTwo"> Sıgn In</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne"> Returns </span>
+          <span className="header_optionLineTwo"> & Orders</span>
+          </div>
+
+          <div className="header_option">
+            <span className="header_optionLineOne"> Your</span>
+            <span className="header_optionLineTwo"> Prime</span>
+          </div>
+          
+          <div className="header_optionBasket">
+           <ShoppingBasketIcon/> 
+           <span className="header_optionLineTwo header_basketCount">0</span>
+          </div>
+        
+        </div>
+      </div>
+    
+  );
+}
+
+export default Header;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+     apiKey: "AIzaSyCUYDEgmpHs-kES9jVGeMlTqflbX9mvus0",
+     authDomain: "challenge-72b87.firebaseapp.com",
+     projectId: "challenge-72b87",
+     storageBucket: "challenge-72b87.appspot.com",
+     messagingSenderId: "237618186488",
+     appId: "1:237618186488:web:d3c36647225d541dd9530d",
+     measurementId: "G-H0VLK4GBQS"
+   };
