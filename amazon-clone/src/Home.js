b63cff3e795f0adcbc866c7781c66f9e47bf7108
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2022-02-03 16:24:58 +0300

    product sayfa başlangıcı

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 function Home() {
   return (
     <div className="home">
       <div className="home_container">
         <img
-        className="home_image"
+          className="home_image"
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt=""
         />
         <div className="home_row">
-             
+          <Product />
+          
         </div>
         <div className="home_row">
-             
+          
         </div>
         <div className="home_row">
              
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import "./Product.css"
+import React from "react";
+import "./Product.css";
 
 function Product() {
-  return( 
-  <div className='product'>
+  return (
+    <div className="product">
+      <div className="product_info">
+        <p>The Lean Startup</p>
+        <p className="product_price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
 
-  </div>);
+        <div className="product_rating">
+          <p>⭐️</p>
+        </div>
+      </div>
+
+      <img src="https://r.resimlink.com/5axsoV.jpg" alt="" />
+
+      <button>Add to Basket</button>
+    </div>
+  );
 }
 
 export default Product;
