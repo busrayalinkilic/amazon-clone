f647e6d37a334b482f0348bf2f0a01b787a836c9
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2022-06-08 19:43:19 +0300

    checkout page-1

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket= () => {
+
+  }
+
   return (
     <div className="product">
       <div className="product_info">
@@ -23,7 +30,7 @@ function Product({ id, title, image, price, rating }) {
 
       <img src={image} alt="" />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
