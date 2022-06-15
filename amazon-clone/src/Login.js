98635f500b8d4947e6bb25dfcb059d3680f35004
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2022-06-15 14:09:48 +0300

    login page-2

--- amazon-clone/src/Login.js
+++ amazon-clone/src/Login.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="login">
       <Link to="/">
@@ -18,26 +20,26 @@ function Login() {
           <h5>E-mail</h5>
           <input
             type="text"
-            
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <h5>Password</h5>
           <input
             type="password"
-            
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
 
-          <button>         
-            Sign In
-          </button>
+          <button className="login__signInButton">Sign In</button>
         </form>
 
         <p>
-          By signing-in you agree to the AMAZON CLONE BY&AAT. Please see our Privacy Notice, our Cookies Notice and our
-          Interest-Based Ads Notice.
+          By signing-in you agree to the AMAZON CLONE BY&AAT. Please see our
+          Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
         </p>
 
-        <button>
+        <button className="login__registerButton">
           Create your Amazon Account
         </button>
       </div>
