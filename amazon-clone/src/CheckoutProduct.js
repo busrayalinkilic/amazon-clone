d76db7596ef9786129c1eca44a9a0485b189de6e
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2022-06-15 12:12:17 +0300

    checkout page-4

--- amazon-clone/src/CheckoutProduct.js
+++ amazon-clone/src/CheckoutProduct.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
@@ -19,7 +28,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <p>🌟</p>
             ))}
         </div>
-        <button>Remove from Basket</button>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
     </div>
   );
