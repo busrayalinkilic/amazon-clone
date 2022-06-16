214294b1e3abf99d11c83f0addc14e4c4632e01a
Büşra Yalınkılıç <1030516778@erciyes.edu.tr>
2022-06-17 01:22:45 +0300

    user authentication2

--- amazon-clone/src/Login.js
+++ amazon-clone/src/Login.js
@@ -1,28 +1,35 @@
 import React, { useState } from "react";
 import "./Login.css";
 import { Link, useHistory } from "react-router-dom";
-import {auth} from "./firebase";
+import { auth } from "./firebase";
 
 function Login() {
-  const  history = useHistory();
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const signIn = (e) => {
     e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
   };
 
   const register = (e) => {
     e.preventDefault();
     auth
-    .createUserWithEmailAndPassword(email, password)
-    .then((auth) => {
-      console.log(auth);
-      if (auth){
-        history.push("/")
-      }
-    })
-    .catch(error => alert(error.message))
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth);
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
   };
 
   return (
